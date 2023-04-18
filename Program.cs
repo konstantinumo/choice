4cb83c7cd11475a5ba8c00420eb558a5307c525b
konstantin <konst.geekbrains@mail.ru>
2023-04-18 13:04:18 +0300

    добавлен текст программы

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string[] s = new string[5] {"skufekuae", "hhk", "p98w3yr8", "91", "j"};
+string[] p = new string[5] {"", "", "", "", ""};
+
+Console.WriteLine("исходный массив строк:");
+
+for(int i=0; i<5; i++)
+Console.WriteLine("s[" + i + "]: " + s[i]);
+
+Console.WriteLine("новый массив строк, длиной не более 3х символов:");
+
+int j = 0;
+
+for(int i=0; i<5; i++)
+{
+    if(s[i].Length<4)
+    {
+        p[j]=s[i];
+        Console.WriteLine("p[" + j + "]: " + p[j]);
+        j++;
+    }
+}
+
